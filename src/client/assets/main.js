d4c377fe3ccf832277c3d9584c3d1d9a80552a36
Jason Bowden <jason.bowden1000@gmail.com>
2017-07-24 06:17:32 -0500

    Added a bunch of stuff

---
+++ src/client/assets/main.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+  let chartContainer = document.getElementById('chartContainer');
+  let dataPoints = JSON.parse(chartContainer.dataset.scrawl);
+
+  var chart = new CanvasJS.Chart("chartContainer", {
+    theme: "theme2",
+    title: {
+      text: "Opening Scrawl Lengths"
+    },
+    height: 400,
+    axisX: {
+      labelAutoFit: false,
+      labelFontSize: 12,
+      labelMaxWidth: 60,
+      labelWrap: true
+    },
+    axisY: {
+      minimum: 400,
+      title: "Scrawl Length"
+    },
+    zoomEnabled: true,
+    data: [{
+      type: "column",
+      dataPoints: dataPoints
+    }]
+  });
+
+  chart.render();
+};

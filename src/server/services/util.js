bbd4130e70cde877766d9a18beebfbef8d03dda9
Jason Bowden <jason.bowden1000@gmail.com>
2017-07-26 15:07:32 -0500

    Added semi-colon

--- src/server/services/util.js
+++ src/server/services/util.js
@@ -6,7 +6,7 @@ function shuffle(a) {
   for (let i = a.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     let temp = a[i];
-    a[i] = a[j]
+    a[i] = a[j];
     a[j] = temp;
   }
 
